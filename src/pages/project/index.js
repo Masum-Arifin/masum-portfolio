a9329e41c2bf88ac0fa22a6062d67401d3ea4e26
Masum-Arifin <you@example.com>
2022-12-26 14:46:51 +0600

    new code

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -51,89 +51,89 @@ export const projects = [
 
     gitHubUrl: {
       icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/OpenAI-project',
+      url: 'https://github.com/Masum-Arifin/open-ai',
     },
     website: {
       icon: <TbWorld />,
       url: 'https://gpt3-openai-project.netlify.app/',
     },
   },
-  {
-    id: 4,
-    name: 'Hotel Booking Project',
-    desc: 'A complicated project that enhanced my skills a lot',
-    stack: 'React JS',
-    gitHubUrl: {
-      icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/React-booking-project',
-    },
-    website: {
-      icon: <TbWorld />,
-      url: 'https://react-booking-project.netlify.app/',
-    },
-    imgUrl:
-      'https://cdn.dribbble.com/userupload/2512424/file/original-03f9f265f9a4adbd54644841930518a1.png?compress=1&resize=320x240&vertical=top',
-  },
+  // {
+  //   id: 4,
+  //   name: 'Hotel Booking Project',
+  //   desc: 'A complicated project that enhanced my skills a lot',
+  //   stack: 'React JS',
+  //   gitHubUrl: {
+  //     icon: <GrGithub />,
+  //     url: 'https://github.com/Almarex-Web-Dev/React-booking-project',
+  //   },
+  //   website: {
+  //     icon: <TbWorld />,
+  //     url: 'https://react-booking-project.netlify.app/',
+  //   },
+  //   imgUrl:
+  //     'https://cdn.dribbble.com/userupload/2512424/file/original-03f9f265f9a4adbd54644841930518a1.png?compress=1&resize=320x240&vertical=top',
+  // },
 
-  {
-    id: 5,
-    name: 'Rest Country API',
-    desc: 'A project that displays all information about a country',
-    stack: 'React JS',
-    gitHubUrl: {
-      icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/rest-countries-api-project',
-    },
-    website: {
-      icon: <TbWorld />,
-      url: 'https://rest-countries-web-app-project.netlify.app/',
-    },
-    imgUrl: restCOuntry,
-  },
-  {
-    id: 6,
-    name: 'Huddle Landing Page',
-    desc: 'A simple landing page website',
-    stack: 'React js, SCSS',
-    gitHubUrl: {
-      icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/Huddle-landing-page',
-    },
-    website: {
-      icon: <TbWorld />,
-      url: 'https://huddle-website-project.netlify.app/',
-    },
-    imgUrl: huddle,
-  },
-  {
-    id: 4,
-    name: 'Hotel Booking Project',
-    desc: 'A complicated project that enhanced my skills a lot',
-    stack: 'React JS',
-    gitHubUrl: {
-      icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/React-booking-project',
-    },
-    website: {
-      icon: <TbWorld />,
-      url: 'https://react-booking-project.netlify.app/',
-    },
-    imgUrl:
-      'https://cdn.dribbble.com/userupload/2512424/file/original-03f9f265f9a4adbd54644841930518a1.png?compress=1&resize=320x240&vertical=top',
-  },
-  {
-    id: 2,
-    name: 'Restaurant Website',
-    desc: 'A Responsive restaurant website design with great UI Effect',
-    stack: 'React JS',
-    gitHubUrl: {
-      icon: <GrGithub />,
-      url: 'https://github.com/Almarex-Web-Dev/Restaurant-Website-project',
-    },
-    website: {
-      icon: <TbWorld />,
-      url: 'https://gericht-restaurant-website.netlify.app/',
-    },
-    imgUrl: gericht,
-  },
+  // {
+  //   id: 5,
+  //   name: 'Rest Country API',
+  //   desc: 'A project that displays all information about a country',
+  //   stack: 'React JS',
+  //   gitHubUrl: {
+  //     icon: <GrGithub />,
+  //     url: 'https://github.com/Almarex-Web-Dev/rest-countries-api-project',
+  //   },
+  //   website: {
+  //     icon: <TbWorld />,
+  //     url: 'https://rest-countries-web-app-project.netlify.app/',
+  //   },
+  //   imgUrl: restCOuntry,
+  // },
+  // {
+  //   id: 6,
+  //   name: 'Huddle Landing Page',
+  //   desc: 'A simple landing page website',
+  //   stack: 'React js, SCSS',
+  //   gitHubUrl: {
+  //     icon: <GrGithub />,
+  //     url: 'https://github.com/Almarex-Web-Dev/Huddle-landing-page',
+  //   },
+  //   website: {
+  //     icon: <TbWorld />,
+  //     url: 'https://huddle-website-project.netlify.app/',
+  //   },
+  //   imgUrl: huddle,
+  // },
+  // {
+  //   id: 4,
+  //   name: 'Hotel Booking Project',
+  //   desc: 'A complicated project that enhanced my skills a lot',
+  //   stack: 'React JS',
+  //   gitHubUrl: {
+  //     icon: <GrGithub />,
+  //     url: 'https://github.com/Almarex-Web-Dev/React-booking-project',
+  //   },
+  //   website: {
+  //     icon: <TbWorld />,
+  //     url: 'https://react-booking-project.netlify.app/',
+  //   },
+  //   imgUrl:
+  //     'https://cdn.dribbble.com/userupload/2512424/file/original-03f9f265f9a4adbd54644841930518a1.png?compress=1&resize=320x240&vertical=top',
+  // },
+  // {
+  //   id: 2,
+  //   name: 'Restaurant Website',
+  //   desc: 'A Responsive restaurant website design with great UI Effect',
+  //   stack: 'React JS',
+  //   gitHubUrl: {
+  //     icon: <GrGithub />,
+  //     url: 'https://github.com/Almarex-Web-Dev/Restaurant-Website-project',
+  //   },
+  //   website: {
+  //     icon: <TbWorld />,
+  //     url: 'https://gericht-restaurant-website.netlify.app/',
+  //   },
+  //   imgUrl: gericht,
+  // },
 ]
